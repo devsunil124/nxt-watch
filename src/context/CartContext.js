1c6c383a7d2a3c19ba5f6f6bc8866370e238b0e3
devsunil124 <kunapareddysunilkumar124@gmail.com>
2023-09-16 14:00:29 +0000

    first commit

---
+++ src/context/CartContext.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const CartContext = React.createContext({
+  isDarkTheme: false,
+  savedVideos: [],
+  addToSaveVideos: () => {},
+  activeTabItem: () => {},
+  activeTab: '',
+  onChangeTheme: () => {},
+  removeSaveVideos: () => {},
+})
+
+export default CartContext
+
+
